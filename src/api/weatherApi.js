f1b14838dee9db584648a2ac00a5f4c94a332252
J.Boughattas <boughattasjassem@outlook.com>
2022-10-12 19:11:51 +0100

    fix typo

--- src/api/weatherApi.js
+++ src/api/weatherApi.js
@@ -4,7 +4,7 @@ const APP_ID = "51ee09bab3648e0efadd4548d24d370e";
 const BASE_URL = "https://api.openweathermap.org/data/2.5";
 
 export const fetchWeatherData = async (position) => {
-  const PARAMS = `weather?lat=${position.lat}&lon=${position.lon}$appid=${APP_ID}`;
+  const PARAMS = `weather?lat=${position.lat}&lon=${position.lon}&appid=${APP_ID}`;
   const toFetch = `${BASE_URL}/${PARAMS}`;
 
   const response = await axios.get(toFetch);
