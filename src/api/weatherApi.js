9618196c72304fa9a0d91fe7e00395f195a7c225
J.Boughattas <boughattasjassem@outlook.com>
2022-10-13 13:04:46 +0100

    hide appid & add unit param

--- src/api/weatherApi.js
+++ src/api/weatherApi.js
@@ -1,10 +1,10 @@
 import axios from "axios";
 
-const APP_ID = "51ee09bab3648e0efadd4548d24d370e";
+const APP_ID = process.env.APPID;
 const BASE_URL = "https://api.openweathermap.org/data/2.5";
 
 export const fetchWeatherData = async (position) => {
-  const PARAMS = `weather?lat=${position.lat}&lon=${position.lon}&appid=${APP_ID}`;
+  const PARAMS = `weather?lat=${position.lat}&lon=${position.lon}&appid=${APP_ID}&units=metric`;
   const toFetch = `${BASE_URL}/${PARAMS}`;
 
   const response = await axios.get(toFetch);
