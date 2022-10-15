16a7d620c79650fd64220a9fd40696db0c7f100d
J.Boughattas <boughattasjassem@outlook.com>
2022-10-15 10:06:04 +0100

    use env variable for app id

--- src/api/weatherApi.js
+++ src/api/weatherApi.js
@@ -1,10 +1,10 @@
 import axios from "axios";
 
-const APP_ID = process.env.APPID;
+const APP_ID = process.env.APP_ID;
 const BASE_URL = "https://api.openweathermap.org/data/2.5";
 
-export const fetchWeatherData = async (position) => {
-  const PARAMS = `weather?lat=${position.lat}&lon=${position.lon}&appid=${APP_ID}&units=metric`;
+export const fetchWeatherData = async (position, unit) => {
+  const PARAMS = `weather?lat=${position.lat}&lon=${position.lon}&appid=${APP_ID}&units=${unit}`;
   const toFetch = `${BASE_URL}/${PARAMS}`;
 
   const response = await axios.get(toFetch);
