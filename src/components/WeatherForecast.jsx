2403ab5584eef3c090cd2d816c64325691c7a683
J.Boughattas <boughattasjassem@outlook.com>
2022-10-16 15:34:29 +0100

    make weather forecast draggable

--- src/components/WeatherForecast.jsx
+++ src/components/WeatherForecast.jsx
@@ -1,10 +1,15 @@
-import { useEffect, useState } from "react";
+import { useState, useRef, useEffect } from "react";
 import { useSelector } from "react-redux";
+import { motion } from "framer-motion";
+
 import WeatherIcon from "./WeatherIcon";
 
 import { getDay, formatAMPM } from "../utils/date";
 
 const WeatherForecast = () => {
+  const [width, setWidth] = useState(0);
+  const carousel = useRef();
+
   const weatherForecastData = useSelector(
     (state) => state.weather.weatherForecastData
   );
@@ -15,19 +20,23 @@ const WeatherForecast = () => {
     (state) => state.weather.weatherForecastError
   );
 
-  const [days, setDays] = useState([]);
-
-  useEffect(() => {
-    setDays(
-      weatherForecastData?.list
-        ?.map?.((item) => getDay(item?.dt_txt))
-        ?.filter?.((item, i, arr) => arr.indexOf(item) === i)
-    );
-  }, []);
+  useEffect(
+    () =>
+      setWidth(carousel?.current?.scrollWidth - carousel?.current?.offsetWidth),
+    [weatherForecastData]
+  );
 
   return (
-    <div className="flex flex-col items-center select-none">
-      <div className="flex py-4 overflow-x-scroll h-10rem  w-[100vw] max-w-[50rem]">
+    <motion.div
+      className="flex flex-col items-center overflow-x-hidden select-none rounded-md ring-1 ring-gray-200 dark:ring-gray-700"
+      ref={carousel}
+    >
+      <motion.div
+        className="flex py-4 h-10rem cursor-grab w-[100vw] max-w-[50rem]"
+        whileDrag={{ cursor: "grabbing" }}
+        drag="x"
+        dragConstraints={{ right: 0, left: -width }}
+      >
         {weatherForecastData?.list.map((item, i) => (
           <div
             key={i}
@@ -37,7 +46,7 @@ const WeatherForecast = () => {
               {formatAMPM(new Date(item?.dt_txt))}{" "}
               {getDay(item?.dt_txt).slice(0, 3)}
             </div>
-            <div className="p-1">
+            <div className="p-1 pointer-events-none">
               <WeatherIcon
                 id={item?.weather[0]?.id}
                 icon={item?.weather[0]?.icon}
@@ -49,13 +58,13 @@ const WeatherForecast = () => {
             </div>
           </div>
         ))}
-      </div>
+      </motion.div>
       {weatherForecastStatus === "loading" ? (
         <div className=" absolute bottom-[6rem] dark:text-gray-700 text-gray-300">
           loading...
         </div>
       ) : null}
-    </div>
+    </motion.div>
   );
 };
 
